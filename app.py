0e9a6c2d961560759974846e4d806c9b0e1ee8e4
Ajay <ajaysajikumar8@gmail.com>
2023-05-28 00:13:52 +0530

    add modification files

--- app.py
+++ app.py
@@ -1,9 +1,48 @@
 from flask import Flask, render_template, request, jsonify
 from chat import get_response
-from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from forms import LoginForm, RegisterForm
+
 
 app = Flask(__name__)
-CORS(app)
+app.config['SECRET_KEY'] = "u}k@a5ThOTA5llx"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///bookclub.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.app_context().push()
+db = SQLAlchemy(app=app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db.session.query(User).get(user_id)
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(250), nullable = False)
+    password = db.Column(db.String(250), nullable = False)
+
+
+
+class Book(db.Model):
+    __tablename__ = "books"
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(250), nullable = False)
+    author = db.Column(db.String(250), nullable = False)
+    genre = db.Column(db.String(250), nullable = False)
+    rating = db.Column(db.Float(), nullable=False)
+    price = db.Column(db.Float(), nullable = False)
+    img_url = db.Column(db.String(), nullable = False)
+
+
+db.create_all()
 
 
 @app.get("/")
